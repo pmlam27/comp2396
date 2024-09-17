5b87020c51235bcbd2dd11a7a4632bc46b7d0a5e
pmlam27 <p.m.benlam@gmail.com>
2024-09-17 17:38:35 +0800

    COMP2396: added JavaGUI

---
+++ comp2396/JavaGUI/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        frame.setSize(420, 420);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        System.out.print("frame initialized");
+    }
+}

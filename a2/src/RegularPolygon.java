d53bd479f8e7a950270fe2b64d47013981780b86
pmlam27 <p.m.benlam@gmail.com>
2024-09-28 21:07:51 +0800

    a2: added basic code structure

---
+++ a2/src/RegularPolygon.java
@@ -0,0 +1,40 @@
+public class RegularPolygon {
+    private int numOfSides;
+    private double radius;
+
+    public RegularPolygon(int n, double r) {
+
+    }
+
+    public RegularPolygon(int n) {
+
+    }
+
+    public RegularPolygon() {
+
+    }
+
+    // TODO: decide public or private
+    int getNumOfSides() {
+        return numOfSides;
+    }
+
+    // TODO: decide public or private
+    double getRadius() {
+        return radius;
+    }
+
+    // TODO: decide public or private
+    void setNumOfSides(int numOfSides) {
+        this.numOfSides = numOfSides;
+    }
+
+    // TODO: decide public or private
+    void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    void setVertices() {
+
+    }
+}
---
+++ a2/src/Shape.java
@@ -0,0 +1,83 @@
+import java.awt.Color;
+
+public class Shape {
+    private Color color;
+    private boolean filled;
+    private double theta;
+    private double xc;
+    private double yc;
+    private double[] xLocal;
+    private double[] yLocal;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public boolean getFilled() {
+        return filled;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public double getXc() {
+        return xc;
+    }
+
+    public double getYc() {
+        return yc;
+    }
+
+    public double[] getxLocal() {
+        return xLocal;
+    }
+
+    public double[] getyLocal() {
+        return yLocal;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
+    public void setXc(double xc) {
+        this.xc = xc;
+    }
+
+    public void setYc(double yc) {
+        this.yc = yc;
+    }
+
+    public void setxLocal(double[] xLocal) {
+        this.xLocal = xLocal;
+    }
+
+    public void setyLocal(double[] yLocal) {
+        this.yLocal = yLocal;
+    }
+
+    public void translate(double dx, double dy) {
+
+    }
+
+    public void rotate(double[] dt) {
+
+    }
+
+    public int[] getX() {
+        return new int[] {};
+    }
+
+    public int[] getY() {
+        return new int[] {};
+    }
+}

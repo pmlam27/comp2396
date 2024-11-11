783a42d7acb897052bbda969e28d4d9fb68b749f
pmlam27 <p.m.benlam@gmail.com>
2024-11-11 14:32:21 +0800

    added more panel

--- a4/src/BigTwoGUI.java
+++ a4/src/BigTwoGUI.java
@@ -3,8 +3,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class BigTwoGUI implements CardGameUI {
-    static int frameWidth = 600;
-    static int frameHeight = 600;
+    static int frameWidth = 800;
+    static int frameHeight = 800;
     JFrame frame;
     JPanel bigTwoPanel;
 
@@ -14,6 +14,7 @@ public class BigTwoGUI implements CardGameUI {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(frameWidth, frameHeight);
+        frame.setMinimumSize(new Dimension(300, 300));
 
         frame.add(bigTwoPanel);
         frame.setVisible(true);
@@ -22,26 +23,128 @@ public class BigTwoGUI implements CardGameUI {
     public static class BigTwoPanel extends JPanel {
         BigTwoPanel() {
             setLayout(new GridBagLayout());
+
+            /*
+            leftPanel (x=0, y=0-1)
+            topRightPanel (x=1, y=0)
+            bottomRightPanel (x=1, y=1)
+            bottomBar (x=0-1, y=2)
+            */
+            LeftPanel leftPanel = new LeftPanel();
+            add(leftPanel, leftPanel.gbc);
+
+            JPanel topRightPanel = new JPanel();
+            topRightPanel.setBackground(Color.lightGray);
+            topRightPanel.setBorder(BorderFactory.createLineBorder(Color.darkGray));
+
+            GridBagConstraints topRightPanelConfig = new GridBagConstraints();
+            topRightPanelConfig.fill = GridBagConstraints.BOTH;
+            topRightPanelConfig.gridx = 1;
+            topRightPanelConfig.gridy = 0;
+            topRightPanelConfig.gridheight = 1;
+            topRightPanelConfig.gridwidth = 1;
+            topRightPanelConfig.weightx = 3;
+            topRightPanelConfig.weighty = 8;
+
+            add(topRightPanel, topRightPanelConfig);
+
+            JPanel bottomRightPanel = new JPanel();
+            bottomRightPanel.setBackground(Color.lightGray);
+            bottomRightPanel.setBorder(BorderFactory.createLineBorder(Color.darkGray));
+
+            GridBagConstraints bottomRightPanelConfig = new GridBagConstraints();
+            bottomRightPanelConfig.fill = GridBagConstraints.BOTH;
+            bottomRightPanelConfig.gridx = 1;
+            bottomRightPanelConfig.gridy = 1;
+            bottomRightPanelConfig.gridheight = 1;
+            bottomRightPanelConfig.gridwidth = 1;
+            bottomRightPanelConfig.weightx = 3;
+            bottomRightPanelConfig.weighty = 8;
+
+            add(bottomRightPanel, bottomRightPanelConfig);
+
+            BottomBar bottomBar = new BottomBar();
+            add(bottomBar, bottomBar.gbc);
+        }
+    }
+
+    public static class BottomBar extends JPanel {
+        public GridBagConstraints gbc = new GridBagConstraints();
+
+        public BottomBar() {
+            // gbc to be used by BigTwoPanel
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.gridx = 0;
+            gbc.gridy = 2;
+            gbc.gridheight = 1;
+            gbc.gridwidth = 2;
+            gbc.weightx = 1;
+            gbc.weighty = 1;
+
+            setBorder(BorderFactory.createLineBorder(Color.darkGray));
+
+            JButton playButton = new JButton();
+            playButton.setText("Play");
+
+            JButton passButton = new JButton();
+            passButton.setText("Pass");
+
+            JLabel messagePrompt = new JLabel();
+            messagePrompt.setText("Message: ");
+
+            JTextField messageInput = new JTextField();
+
+            add(playButton);
+            add(passButton);
+            add(messagePrompt);
+            add(messageInput);
+        }
+    }
+
+    public static class LeftPanel extends JPanel {
+
+        public GridBagConstraints gbc = new GridBagConstraints();
+
+        public LeftPanel() {
+            // gbc to be used by BigTwoPanel
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.gridheight = 2;
+            gbc.gridwidth = 1;
+            gbc.weightx = 4;
+            gbc.weighty = 16;
+
+            setLayout(new GridBagLayout());
             GridBagConstraints gbc = new GridBagConstraints();
             gbc.fill = GridBagConstraints.BOTH;
-
-            JPanel leftPanel = new JPanel();
-            leftPanel.setBackground(Color.lightGray);
             gbc.weightx = 1;
             gbc.weighty = 1;
             gbc.gridx = 0;
-            gbc.gridy = 0;
-            add(leftPanel, gbc);
 
-            JPanel rightPanel = new JPanel();
-            rightPanel.setBackground(Color.white);
-            gbc.weightx = 2;
-            gbc.weighty = 1;
-            gbc.gridx = 1;
-            gbc.gridy = 0;
-            add(rightPanel, gbc);
+            PlayerPanel[] panelList = {
+                    new PlayerPanel(), new PlayerPanel(), new PlayerPanel(),
+                    new PlayerPanel(), new PlayerPanel()
+            };
 
-            setBackground(Color.white);
+            for (int i=0; i<5; i++) {
+                gbc.gridy = i;
+                panelList[i].setBorder(BorderFactory.createLineBorder(Color.darkGray));
+                add(panelList[i], gbc);
+            }
+        }
+    }
+
+    public static class PlayerPanel extends JPanel {
+        public PlayerPanel() {
+            JButton button = new JButton();
+            button.setText("Hi");
+            add(button);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
         }
     }
 
